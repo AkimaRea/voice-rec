fe1978e1d39103338258003a29b3b29513e195b6
AkimaRea <140754658+AkimaRea@users.noreply.github.com>
2024-06-18 21:05:15 +0300

    some tests

---
+++ test.py
@@ -0,0 +1,25 @@
+from gtts import gTTS
+from io import BytesIO
+import pygame
+
+# Текст для озвучки
+text = "Привет, это пример озвучки текста без генерации файла с использованием библиотеки gTTS."
+
+# Создание объекта gTTS и передача текста
+tts = gTTS(text, lang='ru')
+
+# Использование BytesIO для хранения аудиоданных в памяти
+audio_data = BytesIO()
+tts.write_to_fp(audio_data)
+
+# Инициализация Pygame
+pygame.mixer.init()
+audio_data.seek(0)
+pygame.mixer.music.load(audio_data)
+pygame.mixer.music.play()
+
+# Ожидание завершения воспроизведения
+while pygame.mixer.music.get_busy():
+    pygame.time.Clock().tick(10)
+
+print("Текст успешно озвучен.")
