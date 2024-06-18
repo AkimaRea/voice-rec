8f332007b832c2ce4677e3794310e60ad42a6f4a
AkimaRea <140754658+AkimaRea@users.noreply.github.com>
2024-06-18 22:43:22 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from vosk import Model, KaldiRecognizer
 import pyaudio
-from speakerpy.lib_speak import Speaker
-from speakerpy.lib_sl_text import SeleroText
+
+from response_module import calc_response
+from tts_module import speak_text
 
 model = Model("vosk-model-small-ru-0.22") # полный путь к модели
 rec = KaldiRecognizer(model, 16000)
 p = pyaudio.PyAudio()
+
 stream = p.open(
 		format=pyaudio.paInt16, 
 		channels=1, 
@@ -14,17 +16,9 @@ stream = p.open(
 		frames_per_buffer=16000
 )
 stream.start_stream()
-speaker = Speaker(model_id="v3_1_ru", language="ru", speaker="xenia", device="cpu")
 old_recognized=''
 
-def responseAtText(text): 
-	response = ''
- 
-	if text == 'история':
-		response='Высшее учебное заведение, один из федеральных университетов России, расположенный в Ростове-на-Дону и Таганроге Ростовской области. Является крупным научно-образовательным центром России'
-  
-	if response != '':
-		speaker.speak(text=response, sample_rate=48000, put_accent=True, put_yo=False)
+""" speak_text(text="Привет, я голосовой помощник, разработан студентом Института Радиотехнических Систем и Управления кафедры САУ, готова ответить на интересующие вопросы") """
 
 while True:
 		data = stream.read(16000)
@@ -38,6 +32,4 @@ while True:
 		else: continue 
   
 		print(old_recognized)
-		if old_recognized: responseAtText(old_recognized)
-
-  
+		if old_recognized: calc_response(old_recognized)
