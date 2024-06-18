3c58566c28720ecf9b7186480e0db12855757387
AkimaRea <140754658+AkimaRea@users.noreply.github.com>
2024-06-19 00:03:04 +0300

    update

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pyaudio
 from response_module import calc_response
 from tts_module import speak_text
 
-model = Model("vosk-model-small-ru-0.22") # полный путь к модели
+model = Model("vosk-model-small-ru-0.22") # путь к модели
 rec = KaldiRecognizer(model, 16000)
 p = pyaudio.PyAudio()
 
@@ -14,8 +14,10 @@ stream = p.open(
 		rate=16000, 
 		input=True, 
 		frames_per_buffer=16000
-)
+  )
+
 stream.start_stream()
+
 old_recognized=''
 
 """ speak_text(text="Привет, я голосовой помощник, разработан студентом Института Радиотехнических Систем и Управления кафедры САУ, готова ответить на интересующие вопросы") """
