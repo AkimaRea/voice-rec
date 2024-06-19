7e877062290071dc23999ce6a30a3e884eaf8847
AkimaRea <140754658+AkimaRea@users.noreply.github.com>
2024-06-19 03:35:33 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-# Установка пинов
-GPIO_PIN_IN=9
-GPIO_PIN_OUT=10
-
-# Экспортируем пины
-echo $GPIO_PIN_IN > /sys/class/gpio/export
-echo $GPIO_PIN_OUT > /sys/class/gpio/export
-
-# Устанавливаем направление пинов
-echo "in" > /sys/class/gpio/gpio$GPIO_PIN_IN/direction
-echo "out" > /sys/class/gpio/gpio$GPIO_PIN_OUT/direction
+# Инициализация библиотек
+gpio mode 9 in
+gpio mode 10 out
 
 # Функция для выполнения действия при наличии сигнала
 execute_on_signal() {
@@ -22,13 +14,13 @@ execute_on_signal() {
 # Основной цикл для мониторинга GPIO
 while true; do
     # Чтение состояния пина
-    SIGNAL=$(cat /sys/class/gpio/gpio$GPIO_PIN_IN/value)
+    SIGNAL=$(gpio read 9)
     
     if [ "$SIGNAL" -eq "1" ]; then
-        echo "1" > /sys/class/gpio/gpio$GPIO_PIN_OUT/value
+        gpio write 10 1
         execute_on_signal
     else
-        echo "0" > /sys/class/gpio/gpio$GPIO_PIN_OUT/value
+        gpio write 10 0
     fi
 
     # Задержка для предотвращения чрезмерного использования процессора
